4e6422ddff1814760c366a8c6c8efbd083e7ba4e
Bill Hickey <bhickey77@gmail.com>
2018-07-30 20:36:19 -0500

    update pool for heroku postgres

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,24 +1,43 @@
-const pg = require('pg');
-const Pool = pg.Pool;
+// const pg = require('pg');
+// const Pool = pg.Pool;
 
-const DATABASE_NAME = 'listings';
-const config = {
-    database: DATABASE_NAME,
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idelTimeoutMillis: 30000
-}
+// const DATABASE_NAME = process.env.DATA_;
+// const config = {
+//     database: DATABASE_NAME,
+//     host: 'localhost',
+//     port: 5432,
+//     max: 10,
+//     idelTimeoutMillis: 30000
+// }
 
-const pool = new Pool(config);
+// const pool = new Pool(config);
 
-pool.on('connect', client => {
-    console.log(`Connected to database ${DATABASE_NAME}`);
+// pool.on('connect', client => {
+//     console.log(`Connected to database ${DATABASE_NAME}`);
+// });
+
+// pool.on('error', (err, client) => {
+//     console.log(`Error with database connection from ${client}. Error: `, err);
+//     process.exit(-1);
+// });
+
+// module.exports = pool;
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
 
-pool.on('error', (err, client) => {
-    console.log(`Error with database connection from ${client}. Error: `, err);
-    process.exit(-1);
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
 });
 
-module.exports = pool;
+module.exports = client;
